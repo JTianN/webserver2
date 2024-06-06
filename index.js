f7a4f3fd17c7c6c07aa9b0ab8955a2ed02cc4b48
JTianN <104244770+JTianN@users.noreply.github.com>
2024-06-06 10:12:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! เข้าได้เเล้วน้ะจร้า');
 });
 
 app.listen(port, () => {
